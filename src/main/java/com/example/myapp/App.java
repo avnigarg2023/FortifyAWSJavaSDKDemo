ef8153936316d616e2c8d0ba524caa9d645883cd
avnigarg2023 <129446239+avnigarg2023@users.noreply.github.com>
2023-04-13 15:04:52 -0400

    Update App.java

--- src/main/java/com/example/myapp/App.java
+++ src/main/java/com/example/myapp/App.java
@@ -28,17 +28,19 @@ public class App {
 
   public static void main(String[] args) throws IOException {
 
+    Scanner myObj = new Scanner(System.in);
     Region region = Region.US_WEST_2;
     S3Client s3 = S3Client.builder().region(region).build();
 
     String bucket = "jw-testbucket" + System.currentTimeMillis();
-    String key = "WxTWBsdoAw4gRva64Ws7Ue050eiz1o";
+    System.out.println("Enter Key:"); //"WxTWBsdoAw4gRva64Ws7Ue050eiz1o"
+    String key = myObj.nextLine();
     String acl = "public-read-write";
 
     tutorialSetup(s3, bucket, region, acl);
 
     System.out.println("Uploading object...");
-    System.out.println(key);
+    //System.out.println(key);
     System.out.println(acl);
     
 
